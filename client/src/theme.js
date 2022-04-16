b72b03d1b51a6bc3d5fc820486b367ebf4de4b30
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-04-16 12:48:54 -0400

    font styles and color styles

---
+++ client/src/theme.js
@@ -0,0 +1,46 @@
+const theme = {
+  h1: {
+    fontFamily: "ITC Avant Garde Gothic Std",
+    fontStyle: "normal",
+    fontWeight: 600,
+    fontSize: "128px",
+    lineHeight: "154px",
+  },
+  h2: {
+    fontFamily: "ITC Avant Garde Gothic Std",
+    fontStyle: "normal",
+    fontWeight: 600,
+    fontSize: "64px",
+    lineHeight: "77px",
+  },
+  h3: {
+    fontFamily: "ITC Avant Garde Gothic Std",
+    fontStyle: "normal",
+    fontWeight: 400,
+    fontSize: "32px",
+    lineHeight: "38px",
+  },
+  f1: {
+    fontFamily: "ITC Avant Garde Gothic Std",
+    fontStyle: "normal",
+    fontWeight: 400,
+    fontSize: "32px",
+    lineHeight: "38px",
+  },
+  f2: {
+    fontFamily: "ITC Avant Garde Gothic Std",
+    fontStyle: "normal",
+    fontWeight: 400,
+    fontSize: "24px",
+    lineHeight: "29px",
+  },
+  b1: {
+    fontFamily: "ITC Avant Garde Gothic Std",
+    fontStyle: "normal",
+    fontWeight: 400,
+    fontSize: "32px",
+    lineHeight: "38px",
+  },
+};
+
+export default theme;
